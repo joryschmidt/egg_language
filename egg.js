b8e2406e61697cd0e13af55ac53fff261d147292
Jory Schmidt <joryschmidt1988@gmail.com>
2015-07-26 17:25:14 -0400

    began constructing parser

---
+++ egg.js
@@ -0,0 +1,23 @@
+// Making the Egg language with Javascript
+
+function parseExpression(program){
+	program = skipSpace(program);
+	var match, expr;
+	if (match = /^"([^"]*)"/.exec(program))
+		expr = {type: "value", value: match[1]};
+	else if (match = /^\d+\b/.exec(program))
+		expr = {type: "value", value: Number(match[0])};
+	else if (match = /^[^\s(),"]+/.exec(program))
+		expr = {type: "word", name: match[0]};
+	else
+		throw new SyntaxError("Unexpected syntax: " + program);
+
+	return parseApply(expr, program.slice(match[0].length));
+}
+
+function skipSpace(string){
+	var first = string.search(/\S/);
+	if (first == -1)
+		return "";
+	return string.slice(first);
+}
