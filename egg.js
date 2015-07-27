5567e3978bbb6c9ae36441c7dc7ceca96e0b3ed0
Jory Schmidt <joryschmidt1988@gmail.com>
2015-07-26 20:46:09 -0400

    began coding evaluator

--- egg.js
+++ egg.js
@@ -28,15 +28,44 @@ function parseApply(expr, program){
 		return {expr: expr, rest: program};
 
 	program = skipSpace(program.slice(1));
-	expr = {type: apply, operator: expr, args: []};
+	expr = {type: "apply", operator: expr, args: []};
 	while (program[0] != ")"){
 		var arg = parseExpression(program);
 		expr.args.push(arg.expr);
 		program = skipSpace(arg.rest);
-		if (program == ",")
+		if (program[0] == ",")
 			program = skipSpace(program.slice(1));
 		else if (program[0] != ")")
 			throw new SyntaxError("Expected ',' or ')'");
 	}
 	return parseApply(expr, program.slice(1));
 }
+
+function parse(program){
+	var result = parseExpression(program);
+	if (skipSpace(result.rest).length > 0)
+		throw new SyntaxError("Unexpected text after program");
+	return result.expr;
+}
+
+function evaluate(expr, env){
+	switch(expr.type){
+		case "value": return expr.value;
+		case "word":
+			if (expr.name in env)
+				return env[expr.name];
+			else
+				throw new ReferenceError("Undefined variable: " + expr.name);
+		case "apply": 
+			if (expr.operator.type == "word" && expr.operator.name in specialForms)
+				return specialForms[expr.operator.name](expr.args, env);
+			var op = evaluate(expr.operator, env);
+			if (typeOf op != function)
+				throw new TypeError("Applying a non-function");
+			return op.apply(null, expr.args.map(function(arg){
+				return evaluate(arg, env);
+			}));
+	}
+}
+
+var specialForms = Object.create(null);
